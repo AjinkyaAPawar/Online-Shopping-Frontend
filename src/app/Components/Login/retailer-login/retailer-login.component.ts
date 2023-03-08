e8bc44595ab2aaced26ecc6428260fa5d6368190
AjinkyaAPawar <ajinkyapawar25@proton.me>
2023-03-08 12:54:30 +0530

    Angular

---
+++ src/app/Components/Login/retailer-login/retailer-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-retailer-login',
+  templateUrl: './retailer-login.component.html',
+  styleUrls: ['./retailer-login.component.css']
+})
+export class RetailerLoginComponent {
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { UserLoginComponent } from './Components/Login/user-login/user-login.component';
+import { AdminLoginComponent } from './Components/Login/admin-login/admin-login.component';
+import { RetailerLoginComponent } from './Components/Login/retailer-login/retailer-login.component';
+import { UserRegistrationComponent } from './Components/Register/user-registration/user-registration.component';
+import { RetailerRegistrationComponent } from './Components/Register/retailer-registration/retailer-registration.component';
+import { HomeComponent } from './Components/home/home.component';
+import { CategoriesComponent } from './Components/categories/categories.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { RetailerComponent } from './Components/Panel/retailer/retailer.component';
+import { AdminComponent } from './Components/Panel/admin/admin.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    UserLoginComponent,
+    AdminLoginComponent,
+    RetailerLoginComponent,
+    UserRegistrationComponent,
+    RetailerRegistrationComponent,
+    HomeComponent,
+    CategoriesComponent,
+    ProductsComponent,
+    CartComponent,
+    RetailerComponent,
+    AdminComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
