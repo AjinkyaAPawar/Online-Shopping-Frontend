9872708d1cf55128ced03b2da009b4e61603a5fc
AjinkyaAPawar <ajinkyapawar25@proton.me>
2023-03-10 11:04:14 +0530

    updated

--- src/app/Components/Register/user-registration/user-registration.component.ts
+++ src/app/Components/Register/user-registration/user-registration.component.ts
@@ -9,42 +9,42 @@ import { ProductDataService } from '../../products/ProductDataService';
   styleUrls: ['./user-registration.component.css']
 })
 export class UserRegistrationComponent {
-  user:User = new User();
+//   user:User = new User();
 
-  // constructor(private http: HttpClient, private router:Router) {}
+//   // constructor(private http: HttpClient, private router:Router) {}
 
 
-  // userRegistration(credentials: {
-  //    name : string;
-  //    email: string;
-  //    phone:number;
-  //    address:string;
-  //    city:string;
-  //    state: string;
-  //    password: string;
-  //   }){
-  //     let url = `http://localhost:8080/user/register`;
-  //     console.log(credentials);
-  //     this.http.post(url, credentials).subscribe((res) => {
-  //       console.log(res);
-  //       this.router.navigate(['/']);
-  //     });
-  // }
+//   // userRegistration(credentials: {
+//   //    name : string;
+//   //    email: string;
+//   //    phone:number;
+//   //    address:string;
+//   //    city:string;
+//   //    state: string;
+//   //    password: string;
+//   //   }){
+//   //     let url = `http://localhost:8080/user/register`;
+//   //     console.log(credentials);
+//   //     this.http.post(url, credentials).subscribe((res) => {
+//   //       console.log(res);
+//   //       this.router.navigate(['/']);
+//   //     });
+//   // }
 }
 
 
-  handlesubmit(){
-    console.log(this.user);
-    this.user.name = "";
-    this.user.email = "";
-    this.user.phone = "";
-    this.user.password = "";
-  }
-}
+//   handlesubmit(){
+//     console.log(this.user);
+//     this.user.name = "";
+//     this.user.email = "";
+//     this.user.phone = "";
+//     this.user.password = "";
+//   }
+// }
 
-export class User{
-  name!:string;
-  email!:string;
-  phone!:string;
-  password!:string;
-}
+// export class User{
+//   name!:string;
+//   email!:string;
+//   phone!:string;
+//   password!:string;
+// }
